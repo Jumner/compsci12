649768e50a35a0e8097c0f1c43a1860f8db06b14
Jumner <Jumner@debian>
2021-09-13 21:36:30 -0400

    done comments

--- 1unit/website/main.js
+++ 1unit/website/main.js
@@ -1,1 +1,19 @@
-console.log("test");
+window.onload = () => {
+	// Comment Code
+	const codes = [...document.getElementsByClassName('code')];
+	console.table(codes);
+	// CSS
+	codes
+		.filter(code => {
+			return code.classList.contains('css');
+		})
+		.forEach(code => {
+			const lines = [...code.getElementsByTagName('span')];
+			lines.forEach(line => {
+				line.innerHTML = line.innerHTML
+					.replace('/*', '<span class="comment">/*')
+					.replace('*/', '*/</span>');
+				console.log(line.innerHTML);
+			});
+		});
+};
