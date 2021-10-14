c2ed9f64c0c9101e4430ec922238ff8ed9e4eece
Jumner Chromebook <1frankjus@hdsb.ca>
2021-10-14 10:49:10 -0400

    did some exersizes

--- python/week1/spot/spot.py
+++ python/week1/spot/spot.py
@@ -2,19 +2,12 @@ import glob, csv
 
 files = glob.glob('regional-global-daily-*.csv')
 songDict = {}
-
-def getKey(line):
-	result = ""
-	result += line['Track Name']
-	result += ' by '
-	result += line['Artist']
-	return result
-
 for fileName in files:
 	data = open(fileName, 'r')
 	data.readline()
 	for line in csv.DictReader(data):
-		key = getKey(line)
+		key = f'{line["Track Name"]} by {line["Artist"]}'
+		print(key)
 		if key not in songDict:
 			songDict[key] = {}
 		date = '-'.join(fileName.split('.')[0].split('-')[3:])
